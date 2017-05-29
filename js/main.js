1c712c9b90ef2943c91ec07fa74bb54434ad2f0a
Tarik-C <chris.haack@t-online.de>
2017-05-29 16:17:35 +0200

    Implemented a first svg map into box1
    I tried to make it reusable, allthough that is not quite done.
    The function initmap() can be used by you, too.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,66 @@
+//global variables
+
+
+//D3 Map
+function initmap(element) {
+var width = 960,
+    height = 500;
+
+var projection = d3.geo.miller()
+    .scale(100)
+    .translate([width / 2.7 , height / 2])
+    .precision(0.2);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var graticule = d3.geo.graticule();
+
+var svg = d3.select(element).append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("defs").append("path")
+    .datum({type: "Sphere"})
+    .attr("id", "sphere")
+    .attr("d", path);
+
+svg.append("use")
+    .attr("class", "stroke")
+    .attr("xlink:href", "#sphere");
+
+svg.append("use")
+    .attr("class", "fill")
+    .attr("xlink:href", "#sphere");
+
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+d3.json("data/world-countries.json", function(error, world) {
+  if (error) throw error;
+
+  svg.insert("path", ".graticule")
+      .datum(topojson.feature(world, world.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
+
+  svg.insert("path", ".graticule")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .attr("class", "boundary")
+      .attr("d", path);
+      console.log(svg);
+});
+
+};
+
+
 //Helper functions
 
 degToRad = function (num){
 	return num * Math.PI / 180;
-}
+};
 
 //This function calculates the great circle distance between two points,given as an array of arrays. Ex: [[lat1,lon1],[lat2,lon2]]
 function haversine(points){
@@ -17,4 +75,16 @@ function haversine(points){
 	var c = 2 * Math.asin(Math.sqrt(a));
 	var kilometers = (6367 * c); //Radius of earth
 	return kilometers;
+}
+
+//Init
+
+window.onload = init;
+
+function init() {
+    initmap("#svgmap");
+    
+    
+    
+    
 }
